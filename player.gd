c8c5bcff5344e32efd865773d412c4f46449c4ab
DrakeLeLionBlanc <lelionblanc62@gmail.com>
2025-11-22 03:18:41 +0100

    feat: add MovementData Ressources

--- player.gd
+++ player.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const ACCELERATION = 800.0
-const FRICTION = 1000.0
-const JUMP_VELOCITY = -300.0
+@export var movement_data : PlayerMovementData
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var coyote_jump_timer: Timer = $CoyoteJumpTimer
@@ -14,6 +11,7 @@ func _physics_process(delta: float) -> void:
 	
 	var input_axis := Input.get_axis("ui_left", "ui_right")
 	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
 	handle_acceleration(input_axis, delta)
 	update_animation(input_axis)
 	
@@ -27,24 +25,29 @@ func _physics_process(delta: float) -> void:
 
 func apply_gravity(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * movement_data.gravity_scale * delta
 
 func handle_jump():
 	if is_on_floor() or coyote_jump_timer.time_left > 0:
 		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_VELOCITY
+			velocity.y = movement_data.jump_velocity
 			coyote_jump_timer.stop()
 	if not is_on_floor():
-		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
-			velocity.y = JUMP_VELOCITY / 2 
+		if Input.is_action_just_released("ui_accept") and velocity.y < movement_data.jump_velocity / 2:
+			velocity.y = movement_data.jump_velocity / 2 
 			
 func apply_friction(input_axis, delta):
 	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+
+func apply_air_resistance(input_axis, delta):
+		if input_axis == 0 and not is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
+	
 
 func handle_acceleration(input_axis, delta):
 	if input_axis == 0:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
 
 func update_animation(input_axis):
 	if input_axis != 0:
