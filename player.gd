42a17da1265f14299787a37fe15ccc11e5bdfbd2
DrakeLeLionBlanc <lelionblanc62@gmail.com>
2025-11-22 02:48:09 +0100

    feat: add simple coyote jump

--- player.gd
+++ player.gd
@@ -1,25 +1,57 @@
 extends CharacterBody2D
 
+const SPEED = 100.0
+const ACCELERATION = 800.0
+const FRICTION = 1000.0
+const JUMP_VELOCITY = -300.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_jump_timer: Timer = $CoyoteJumpTimer
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	apply_gravity(delta)
+	handle_jump()
+	
+	var input_axis := Input.get_axis("ui_left", "ui_right")
+	apply_friction(input_axis, delta)
+	handle_acceleration(input_axis, delta)
+	update_animation(input_axis)
+	
+	var was_on_floor= is_on_floor()
+	move_and_slide()
+	
+	# Coyote Jump
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
+
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func handle_jump():
+	if is_on_floor() or coyote_jump_timer.time_left > 0:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+			coyote_jump_timer.stop()
+	if not is_on_floor():
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2 
+			
+func apply_friction(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func handle_acceleration(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+func update_animation(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		animated_sprite_2d.play("idle")
+		
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
